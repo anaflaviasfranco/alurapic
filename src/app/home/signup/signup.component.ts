4e3d375ea2daa54786c6196170c491b8c79aac13
Ana Flávia <anaflaviasfranco@gmail.com>
2020-01-27 09:36:03 -0300

    criacao de validador proprio

--- src/app/home/signup/signup.component.ts
+++ src/app/home/signup/signup.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { lowerCaseValidador } from 'src/app/shared/validators/lower-case.validator';
 
 @Component({
     templateUrl: './signup.component.html'
@@ -26,7 +27,8 @@ export class SignUpComponent implements OnInit{
             ]],
             userName: ['',[
                 Validators.required,
-                Validators.pattern(/^[a-z0-9_\-]+$/),
+                lowerCaseValidador,
+                // Validators.pattern(/^[a-z0-9_\-]+$/),
                 Validators.minLength(2),
                 Validators.maxLength(30)
             ]],
